58ba28de6d0fa8776d716845290932c3be839243
STGHMAAV <32195136+STGHMAAV@users.noreply.github.com>
2020-07-01 12:11:37 +0200

    added minmin + mii bug fixed

--- ssrc/src/components/SkinSelector.tsx
+++ ssrc/src/components/SkinSelector.tsx
@@ -5,8 +5,8 @@ export interface SkinSelectorProps {
 }
 
 export interface SkinSelectorState {
-    selected: number;
     numberSkins: number;
+    buttons: JSX.Element[];
 }
 
 export default class SkinSelector extends React.Component<SkinSelectorProps, SkinSelectorState> {
@@ -14,40 +14,60 @@ export default class SkinSelector extends React.Component<SkinSelectorProps, Ski
     constructor(props: Readonly<SkinSelectorProps>) {
         super(props);
 
+        let options = this.buttonsGenerator(this.props.selectedChar, 0);
+
         this.state = {
-            selected: 0,
-            numberSkins: this.props.selectedChar.toLocaleLowerCase().includes('mii') ? 1 : 8
+            numberSkins: options.nSkins,
+            buttons: options.skins
         };
+    }
+
+    /**
+     * Given a character, returns how many skins it has, and the buttons for each skin
+     * @param selectedChar selectedChar: Selected Character for the player
+     * @param selected selected: Which skin has been selected
+     * @returns nSkins: number of skins for the character || skins: buttons for each skin of the character
+     */
+    public buttonsGenerator(selectedChar: string, selected?: number): { nSkins: number, skins: JSX.Element[] } {
+        let numberSkins = selectedChar.toLocaleLowerCase().includes('mii') ? 1 : 8;
+        let buttons = [];
+
+        for (let i = 0; i < numberSkins; ++i) {
+            buttons.push(<input onClick={this.onClick.bind(this)}
+                disabled={i === (selected ? selected : 0)} style={{ opacity: i === 0 ? 0.5 : 1 }}
+                key={`${selectedChar}_${i}`} value={i} type="image" alt={i.toString()}
+                src={require(`./../stockIcons/${selectedChar.toLowerCase()}_${i}.png`)} />);
+        }
+
+        return { nSkins: numberSkins, skins: buttons };
     }
 
     public onClick(ev: React.MouseEvent<HTMLInputElement, MouseEvent>): void {
         ev.preventDefault();
 
-        this.setState({ selected: parseInt(ev.currentTarget.value) });
+        let options = this.buttonsGenerator(this.props.selectedChar, parseInt(ev.currentTarget.value));
+        this.setState({
+            numberSkins: options.nSkins,
+            buttons: options.skins
+        });
     }
+
 
     public componentWillUpdate(nextProps: any) {
         let { selectedChar } = this.props;
         if (nextProps.selectedChar && selectedChar && nextProps.selectedChar !== selectedChar) {
+            
+            let options = this.buttonsGenerator(nextProps.selectedChar, 0);
+
             this.setState({
-                selected: 0,
-                numberSkins: nextProps.selectedChar.toLocaleLowerCase().includes('mii') ? 1 : 8
+                numberSkins: options.nSkins,
+                buttons: options.skins
             });
         }
     }
 
     public render() {
-        let { selectedChar } = this.props;
-        let { selected, numberSkins } = this.state;
-
-        let buttons = [];
-
-        for (let i = 0; i < numberSkins; ++i) {
-            buttons.push(<input onClick={this.onClick.bind(this)}
-                disabled={i === selected} style={{ opacity: i === selected ? 0.5 : 1 }}
-                key={`${selectedChar}_${i}`} value={i} type="image" alt={i.toString()}
-                src={require(`./../stockIcons/${selectedChar.toLowerCase()}_${i}.png`)} />);
-        }
+        let { buttons } = this.state;
 
         return (
             <div className={'skinselector'} >
